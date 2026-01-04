c6331ebbdaa5107af1b97c3e971b2919edee9fea
yatesh.techhub <yatesh898@gmail.com>
2026-01-04 10:32:21 +0530

    backend url update

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,7 +7,7 @@ export const MainContext = createContext()
 export default function Context({ children }) {
 
   const [Projects, setProjects] = useState([])
-  const API_BASE_URL = "https://agency-backend-13.onrender.com"
+  const API_BASE_URL = "https://agency-backend-14.onrender.com"
   const project_URL = "/project"
   const admin_URL = "/admin"
   const ToastNotify = (msg, status) => toast(msg, { type: status == true ? 'success' : 'error' })
